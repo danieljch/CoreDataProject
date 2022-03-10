53716b4552dfae619ab1c29593764a862a3fd19a
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-10 16:58:56 -0400

    candies - countries coreData finished

---
+++ CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 10/3/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 10/3/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
